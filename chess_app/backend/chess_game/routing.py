9c18c7d3954f0f3e74e279f3905deff5904da674
Kalantri007 <vyankykalantri@gmail.com>
2025-05-02 21:04:19 -0700

    Remove all files for a clean start

--- chess_app/backend/chess_game/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import consumers
-
-# Define WebSocket URL patterns
-websocket_urlpatterns = [
-    path('ws/game/<str:game_id>/', consumers.ChessGameConsumer.as_asgi()),
-    path('ws/lobby/', consumers.LobbyConsumer.as_asgi()),
-]
